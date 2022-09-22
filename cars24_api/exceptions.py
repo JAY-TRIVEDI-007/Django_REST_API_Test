36fbac7a2a4ce4e4fb14fe0dd8e68c550c897581
JAY TRIVEDI <jaytrivedi007jb@gmail.com>
2022-09-22 16:38:24 +0530

    exception bugs fixed

--- cars24_api/exceptions.py
+++ cars24_api/exceptions.py
@@ -11,13 +11,14 @@ def api_exception_handler(exc, context):
     # print(f"ERROR Res data: {response.data}")
     # print(f"ERROR Res data: {response.status_code}")
     if response is not None:
+        # Convert list to string
         for key in response.data:
-            if key == 'detail':
-                response.data['msg'] = response.data.get('detail')
-                del response.data['detail']
-            elif type(response.data.get(key)).__name__ == "list" or type(response.data.get(key)).__name__ == "tuple":
+            if type(response.data.get(key)).__name__ == "list" or type(response.data.get(key)).__name__ == "tuple":
                 response.data[key] = response.data[key][0]
-
+        # change 'detail' to 'msg'
+        if response.data.get('detail'):
+            response.data['msg'] = response.data.get('detail')
+            del response.data['detail']
         response.data['success'] = False
 
     return response
